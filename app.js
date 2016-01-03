3a8c692f871bb2679e4b41e3db374c12c9a5e68e
kwhitejr <kwhitejr.edu@gmail.com>
2016-01-02 19:01:59 -1000

    Initial get functions

--- app.js
+++ app.js
@@ -6,6 +6,30 @@ app.get('/', function(req, res) {
   res.send('Hello World!');
 });
 
+app.get('/about', function(req, res) {
+  res.send('We are awesome!');
+});
+
+app.get('/faq', function(req, res) {
+  res.send('What you need?');
+});
+
+app.get('/contact', function(req, res) {
+  res.send('<form>Dat Form Doe</form>');
+});
+
+app.post('/contact', function(req, res) {
+  res.send('Got a POST request');
+});
+
+app.put('/contact', function(req, res) {
+  res.send('Got a PUT request at /user');
+});
+
+app.delete('/contact', function(req, res) {
+  res.send('Got a DELETE request at /user');
+});
+
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
